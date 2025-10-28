09773153e118fe286ffa6c7db78eeee2369610dc
albina_biriukova <Alja.birjukova@gmail.com>
2025-10-29 00:05:45 +0100

    fix img

--- js/galerry.js
+++ js/galerry.js
@@ -1,18 +1,18 @@
 let galleryInput = document.querySelector(".gallery-wrapper");
 
 let images = [
-  "../image/bagery-2.jpg",
-  "../image/bagery-3.jpg",
-  "../image/bagery-4.jpg",
-  "../image/bagery-5.jpg",
-  "../image/bagery-6.jpg",
-  "../image/bagery-7.jpg",
-  "../image/bagery-8.jpg",
-  "../image/bagery-9.jpg",
-  "../image/bagery-1.jpg",
-  "../image/bagery-10.jpg",
-  "../image/bagery-11.jpg",
-  "../image/bagery-12.jpg",
+  "image/bagery-2.jpg",
+  "image/bagery-3.jpg",
+  "image/bagery-4.jpg",
+  "image/bagery-5.jpg",
+  "image/bagery-6.jpg",
+  "image/bagery-7.jpg",
+  "image/bagery-8.jpg",
+  "image/bagery-9.jpg",
+  "image/bagery-1.jpg",
+  "image/bagery-10.jpg",
+  "image/bagery-11.jpg",
+  "image/bagery-12.jpg",
 ];
 
 images.forEach((image) => {
